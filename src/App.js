83ea062ffb40169240bf6c6ce30cc35b68c70a2f
Ekaterina Stolbova <stolbova8621@gmail.com>
2024-02-24 21:34:45 +0700

    начало регистрации

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import MainMenuAutor from './components/autorisation/comp/mainMenuAutor'
+import MainMenuRegistr from './components/registration/comp/mainMenuRegistr'
 
 function App() {
   return (
     <div className="App">
-        <MainMenuAutor />
+        <MainMenuRegistr />
     </div>
   );
 }
---
+++ src/components/registration/comp/mainMenuRegistr.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../../autorisation/autor.css"
+import Logo from "../../../autorisation/comp/logo.js";
+
+class MainMenuRegistr extends React.Component{
+    render(){
+        return(
+            <div className="autorMenu">
+                <Logo />
+            </div>
+        );
+    }
+}
+
+export default MainMenuRegistr;
