28be49965967bef80b011af50aa3c62b78c3e9eb
Ekaterina Stolbova <stolbova8621@gmail.com>
2024-03-05 02:23:03 +0700

    исправлен ховер с картинками

--- src/components/general/header/headerButtons.js
+++ src/components/general/header/headerButtons.js
@@ -1,19 +1,17 @@
 import React from "react";
 import './headercss.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import envelope from './envelope.svg'
-import key from './key.svg'
-import person from './person.svg'
-import { Svg } from "./svg.tsx";
+import {ReactComponent as Envelope} from "./envelope.svg";
+import {ReactComponent as Key} from "./key.svg";
+import {ReactComponent as Person} from "./person.svg";
 
 class HeaderButtons extends React.Component{
     render() {
         return(
             <div className="headerButtons">
-                <Svg id="envelope" className="svg"/>
-                <img className="custom-icon" src={envelope} alt="envelope"/>
-                <img className="custom-icon" src={key} alt="key"/>
-                <img className="custom-icon" src={person} alt="person"/>
+                <Envelope className="custom-icon hoverIcon"/>
+                <Key className="custom-icon hoverIcon"/>
+                <Person className="custom-icon hoverIcon"/>
             </div>
         )
     }
